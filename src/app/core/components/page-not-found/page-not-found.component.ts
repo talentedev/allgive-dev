bdcc8ad46bab4e31e245211825667316ff753e07
Talent <webdev124@outlook.com>
2019-03-19 19:08:34 +0200

    fix 404 page

--- src/app/core/components/page-not-found/page-not-found.component.ts
+++ src/app/core/components/page-not-found/page-not-found.component.ts
@@ -4,7 +4,7 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
 
